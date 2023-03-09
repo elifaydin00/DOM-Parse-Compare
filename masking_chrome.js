bda8495b95a215b2e4cc344389fa953430a2d6e1
elifaydin00 <59412434+elifaydin00@users.noreply.github.com>
2023-03-09 22:49:15 +0300

    added strings to be masked
    created extractedStrings array to store strings to mask in the doc

--- masking_chrome.js
+++ masking_chrome.js
@@ -61,15 +61,45 @@ async function main() {
     // string representation
     //console.log(reporter.report(result));
 
-    const differences = Object.values(groupedDiff);
-    for (let i = 0; i < differences.length; i++) {
-        console.log(`Differences for node ${Object.keys(groupedDiff)[i]}:`);
-        for (let j = 0; j < differences[i].length; j++) {
-            console.log(differences[i][j]);
-        }
-    }
+    console.log("*************\n");
+    const extractedStrings = [];
 
-       
+
+    for (var i = 0; i < diff.length; i++){
+      if(diff[i].message.includes("expected value") && diff[i].message.includes("instead of")){
+        console.log("Parse by regex and push: ", diff[i].message);
+        const match = diff[i].message.match(/expected value '([^']*)' instead of '([^']*)'/);
+        if (match) {
+          extractedStrings.push(match[1], match[2]);
+        }
+      }
+    }
+    console.log(extractedStrings);
+
+    /*Now, convert dom1 and dom2 to strings and mask if given strings exists */
+    const serializer = new XMLSerializer();
+    const str1_doc = serializer.serializeToString(xmlDoc1);
+    
+    const words = str1_doc.split(' ');
+    //change the word into MASKED if it exists in extractedStrings
+    const maskedWords = words.map(word => extractedStrings.includes(word) ? 'MASKED' : word);
+
+    const maskedStr1 = maskedWords.join(' ');
+
+    //console.log(maskedStr1);
+
+    const serializer2 = new XMLSerializer();
+    const str2_doc = serializer.serializeToString(xmlDoc3);
+    
+    const words2 = str2_doc.split(' ');
+    //change the word into MASKED if it exists in extractedStrings
+    const maskedWords2 = words.map(word => extractedStrings.includes(word) ? 'MASKED' : word);
+
+    const maskedStr2 = maskedWords.join(' ');
+
+    //console.log(maskedStr2);
+
+    //values change?? 
 }
 
 main();
